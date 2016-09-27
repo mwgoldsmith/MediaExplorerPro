8210f99c77ccf66704c62386ce742af14312a099
mwgoldsmith <mike.w.goldsmith@gmail.com>
2016-09-27 12:29:36 +0100

    Include default config file

--- projects/vs2015/mxp/config.h
+++ projects/vs2015/mxp/config.h
@@ -1,3 +1,35 @@
 #pragma once
 
-#define PACKAGE_STRING ""
+#define PACKAGE_STRING    ""
+
+#define MEDIALIB_MAJOR    0    
+
+#define MEDIALIB_MINOR    1    
+
+#define MEDIALIB_MICRO    0    
+
+#define HAVE_WIN32        1
+
+#undef HAVE_LINUX
+
+#undef HAVE_DARWIN
+
+#undef HAVE_IOS
+
+#undef HAVE_OSX
+
+#undef HAVE_TVOS
+
+#define CXX11_THREADS      1
+
+#define CXX11_MUTEX        1
+
+#define HAVE_THREAD_LOCAL 1
+
+#undef HAVE_ANDROID
+
+#undef HAVE_LIBJPEG
+
+#define HAVE_TESTS         1
+
+#undef HAVE_PTHREAD
