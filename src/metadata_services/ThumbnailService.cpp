e2403f477fcd849dda1b16d01b7771b3ddaf9e9e
mwgoldsmith <mike.w.goldsmith@gmail.com>
2016-10-27 18:06:45 +0100

    Increase logging in ThumbnailService; update project file

--- src/metadata_services/ThumbnailService.cpp
+++ src/metadata_services/ThumbnailService.cpp
@@ -11,11 +11,12 @@
 #include "Media.h"
 #include "MediaFile.h"
 #include "Metadata.h"
+#include "av/MediaController.h"
+#include "av/StreamType.h"
 #include "mediaexplorer/ILogger.h"
-#include "parser/ParserMedia.h"
-#include <av/MediaController.h>
-#include <av/StreamType.h>
-#include <image/JpegImage.h>
+#include "image/Image.h"
+#include "image/JpegImageContainer.h"
+#include "utils/Filename.h"
 
 bool mxp::ThumbnailService::Initialize() {
   return true;
@@ -32,6 +33,18 @@ uint8_t mxp::ThumbnailService::nbThreads() const {
   //    return nbProcs;
   // Let's make this code thread-safe first :)
   return 1;
+}
+
+std::string GetThumbnailFilename(const mstring& path, const mstring& mrl, const mxp::IImageContainer& container) {
+  auto name = mxp::utils::file::toAbsolutePath(mrl);
+  name = mxp::utils::file::fileName(name);
+
+  auto pos = name.find_last_of('.');;
+  if(pos != std::string::npos)
+    name = name.substr(0, pos + 1);
+
+  name = name + "." + container.GetExtension();
+  return mxp::utils::file::ConcatPath(path, name);
 }
 
 mxp::parser::Task::Status mxp::ThumbnailService::Run(parser::Task & task) {
@@ -51,17 +64,60 @@ mxp::parser::Task::Status mxp::ThumbnailService::Run(parser::Task & task) {
     return parser::Task::Status::Success;
   }
 
-  LOG_INFO( "Generating ", file->mrl(), " thumbnail..." );
-  auto ctx = av::MediaController::CreateContext(file->mrl());
-  av::MediaController::OpenStream(ctx, StreamType::Video, 0);
-  av::MediaController::Seek(ctx, 10.0);
-  auto image = av::MediaController::ReadFrame(ctx);
-  if(image != nullptr) {
-    JpegImage jpeg;
-    auto f = m_ml->GetFileSystem()->CreateFileFromPath(file->mrl());
-    jpeg.save(image, f->GetName() + "." + jpeg.GetExtension());
+  auto filename = file->mrl();
+  LOG_INFO( "Generating thumbnail for: ", filename);
+
+  ImagePtr image = nullptr;
+  JpegImageContainer jpeg;
+
+  auto ctx = av::MediaController::CreateContext(filename);
+  if(ctx != nullptr) {
+    if(av::MediaController::OpenStream(ctx, StreamType::Video, 0)) {
+      auto position = 10.0;
+      if(av::MediaController::Seek(ctx, position)) {
+        image = av::MediaController::ReadFrame(ctx);
+        if(image == nullptr) {
+          LOG_ERROR("Failed to read frame from media.");
+        }
+      } else {
+        LOG_ERROR("Failed to seek to position in media: ", position);
+      }
+    } else {
+      LOG_ERROR("Failed to open Video stream in media.");
+    }
+
+    if(image != nullptr) {
+      uint32_t width;
+      uint32_t height;
+
+      m_ml->GetSettingInt(SettingsKey::ThumbnailHeight, height);
+      m_ml->GetSettingInt(SettingsKey::ThumbnailWidth, width);
+      if(width >= 0 && height >= 0 && width <= 3200 && height <= 3200) {
+        if (!image->Scale(width, height, true)) {
+          LOG_ERROR("Failed to scale image to dimensions: ", width, "x", height);
+        }
+      }
+
+      mstring path;
+      m_ml->GetSettingString(SettingsKey::ThumbnailPath, path);
+      filename = GetThumbnailFilename(path, filename, jpeg);
+    }
   }
-  
-  return parser::Task::Status::Success;
+
+  if(image != nullptr) {
+    if(jpeg.Compress(image) == true) {
+      if(jpeg.Save(filename) == true) {
+        media->SetThumbnail(filename);
+        media->Save();
+        return parser::Task::Status::Success;
+      } else {
+        LOG_ERROR("Failed to save JPEG image as: ", filename);
+      }
+    } else {
+      LOG_ERROR("Failed to compress thumbnail image as JPEG data");
+    }
+  }
+
+  return parser::Task::Status::Fatal;
 }
 
