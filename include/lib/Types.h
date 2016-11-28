4751a4445b4ea0434be6a19e3bbed00add2c5336
mwgoldsmith <mike.w.goldsmith@gmail.com>
2016-11-28 09:24:27 +0000

    Update forward references

--- include/lib/Types.h
+++ include/lib/Types.h
@@ -12,14 +12,19 @@ namespace mxp {
 class Album;
 class AlbumTrack;
 class Artist;
+class DiscovererWorker;
 class Genre;
 class Image;
 class Media;
 class MediaContext;
+class MediaDevice;
 class MediaExplorer;
 class MediaFile;
 class MediaFolder;
+class ModificationNotifier;
 class Movie;
+class Parser;
+class ParserService;
 class Show;
 class ShowEpisode;
 class SqliteConnection;
@@ -27,13 +32,22 @@ class Stream;
 class VideoTrack;
 enum class SettingsKey;
 
+namespace factory {
+class IFileSystem;
+}
+
+namespace fs {
+class IFile;
+class IDirectory;
+}
+
 using MediaContextPtr  = std::shared_ptr<MediaContext>;
 using ImagePtr         = std::shared_ptr<Image>;
 
 typedef SqliteConnection* DBConnection;
 using MediaExplorerPtr = const MediaExplorer*;
 
-}
+} 
 
 #endif /* TYPES_H */
 
