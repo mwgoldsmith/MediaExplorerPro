a73e305ba7c753b70c82da14a633486db0b02140
mwgoldsmith <mike.w.goldsmith@gmail.com>
2016-11-28 01:36:34 +0000

    Set up for precompiled headers

---
+++ include/lib/stdafx.h
@@ -0,0 +1,10 @@
+/*****************************************************************************
+ * Media Explorer
+ *****************************************************************************/
+
+#ifndef MXP_STDAFX_H
+#define MXP_STDAFX_H
+
+#include "mediaexplorer/Precomp.h"
+
+#endif /* MXP_STDAFX_H */
