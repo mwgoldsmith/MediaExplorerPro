1c3ea8522ac7d13e763462c99a0b80a21f35380c
mwgoldsmith <mike.w.goldsmith@gmail.com>
2016-11-28 00:29:57 +0000

    Refactor Artist.cpp

--- src/lib/Artist.cpp
+++ src/lib/Artist.cpp
@@ -15,19 +15,23 @@
 
 namespace mxp {
 
+using mxp::policy::ArtistTable;
 const std::string policy::ArtistTable::Name = "Artist";
 const std::string policy::ArtistTable::PrimaryKeyColumn = "id_artist";
 int64_t Artist::*const policy::ArtistTable::PrimaryKey = &Artist::m_id;
 
 Artist::Artist(MediaExplorerPtr ml, sqlite::Row& row)
-  : m_ml(ml) {
+  : m_ml(ml)
+  , m_id{}
+  , m_nbAlbums{}
+  , m_isPresent{} {
   row >> m_id
-    >> m_name
-    >> m_shortBio
-    >> m_artworkMrl
-    >> m_nbAlbums
-    >> m_mbId
-    >> m_isPresent;
+      >> m_name
+      >> m_shortBio
+      >> m_artworkMrl
+      >> m_nbAlbums
+      >> m_mbId
+      >> m_isPresent;
 }
 
 Artist::Artist(MediaExplorerPtr ml, const std::string& name)
@@ -51,8 +55,7 @@ const std::string &Artist::GetShortBio() const {
 }
 
 bool Artist::SetShortBio(const std::string &shortBio) {
-  static const auto req = "UPDATE " + policy::ArtistTable::Name
-    + " SET shortbio = ? WHERE id_artist = ?";
+  static const auto req = "UPDATE " + ArtistTable::Name + " SET shortbio = ? WHERE " + ArtistTable::PrimaryKeyColumn + " = ?";
   if(sqlite::Tools::ExecuteUpdate(m_ml->GetConnection(), req, shortBio, m_id) == false)
     return false;
   m_shortBio = shortBio;
@@ -64,7 +67,7 @@ std::vector<AlbumPtr> Artist::albums(SortingCriteria sort, bool desc) const {
 }
 
 std::vector<MediaPtr> Artist::media(SortingCriteria sort, bool desc) const {
-  std::string req = "SELECT med.* FROM " + policy::MediaTable::Name + " med "
+  auto req = "SELECT med.* FROM " + policy::MediaTable::Name + " med "
     "INNER JOIN MediaArtistRelation mar ON mar.media_id = med.id_media "
     "WHERE mar.artist_id = ? AND med.is_present = 1 ORDER BY ";
   switch(sort) {
@@ -101,8 +104,7 @@ const std::string& Artist::GetArtworkMrl() const {
 bool Artist::SetArtworkMrl(const std::string& artworkMrl) {
   if(m_artworkMrl == artworkMrl)
     return true;
-  static const std::string req = "UPDATE " + policy::ArtistTable::Name +
-    " SET artwork_mrl = ? WHERE id_artist = ?";
+  static const auto req = "UPDATE " + ArtistTable::Name + " SET artwork_mrl=? WHERE " + ArtistTable::PrimaryKeyColumn + "=?";
   if(sqlite::Tools::ExecuteUpdate(m_ml->GetConnection(), req, artworkMrl, m_id) == false)
     return false;
   m_artworkMrl = artworkMrl;
@@ -113,8 +115,7 @@ bool Artist::updateNbAlbum(int increment) {
   assert(increment != 0);
   assert(increment > 0 || (increment < 0 && m_nbAlbums >= 1));
 
-  static const std::string req = "UPDATE " + policy::ArtistTable::Name +
-    " SET nb_albums = nb_albums + ? WHERE id_artist = ?";
+  static const auto req = "UPDATE " + ArtistTable::Name + " SET nb_albums = nb_albums + ? WHERE " + ArtistTable::PrimaryKeyColumn + "=?";
   if(sqlite::Tools::ExecuteUpdate(m_ml->GetConnection(), req, increment, m_id) == false)
     return false;
   m_nbAlbums += increment;
@@ -122,8 +123,7 @@ bool Artist::updateNbAlbum(int increment) {
 }
 
 std::shared_ptr<Album> Artist::unknownAlbum() {
-  static const std::string req = "SELECT * FROM " + policy::AlbumTable::Name +
-    " WHERE artist_id = ? AND title IS NULL";
+  static const auto req = "SELECT * FROM " + policy::AlbumTable::Name + " WHERE artist_id=? AND title IS NULL";
   auto album = Album::Fetch(m_ml, req, m_id);
   if(album == nullptr) {
     album = Album::CreateUnknownAlbum(m_ml, this);
@@ -142,8 +142,7 @@ const std::string& Artist::musicBrainzId() const {
 }
 
 bool Artist::setMusicBrainzId(const std::string& mbId) {
-  static const std::string req = "UPDATE " + policy::ArtistTable::Name
-    + " SET mb_id = ? WHERE id_artist = ?";
+  static const auto req = "UPDATE " + ArtistTable::Name + " SET mb_id=? WHERE " + ArtistTable::PrimaryKeyColumn + "=?";
   if(mbId == m_mbId)
     return true;
   if(sqlite::Tools::ExecuteUpdate(m_ml->GetConnection(), req, mbId, m_id) == false)
@@ -154,7 +153,7 @@ bool Artist::setMusicBrainzId(const std::string& mbId) {
 
 bool Artist::CreateTable(DBConnection connection) noexcept {
   static const auto req = "CREATE TABLE IF NOT EXISTS " +
-    policy::ArtistTable::Name +
+    ArtistTable::Name +
     "("
     "id_artist INTEGER PRIMARY KEY AUTOINCREMENT,"
     "name TEXT COLLATE NOCASE UNIQUE ON CONFLICT FAIL,"
@@ -169,7 +168,7 @@ bool Artist::CreateTable(DBConnection connection) noexcept {
     "artist_id INTEGER,"
     "PRIMARY KEY (media_id, artist_id),"
     "FOREIGN KEY(media_id) REFERENCES " + policy::MediaTable::Name + "(" + policy::MediaTable::PrimaryKeyColumn + ") ON DELETE CASCADE,"
-    "FOREIGN KEY(artist_id) REFERENCES " + policy::ArtistTable::Name + "("+ policy::ArtistTable::PrimaryKeyColumn + ") ON DELETE CASCADE"
+    "FOREIGN KEY(artist_id) REFERENCES " + ArtistTable::Name + "("+ ArtistTable::PrimaryKeyColumn + ") ON DELETE CASCADE"
     ")";
 #ifdef USE_FTS
   static const auto reqFts = "CREATE VIRTUAL TABLE IF NOT EXISTS " +
@@ -199,20 +198,20 @@ bool Artist::CreateTriggers(DBConnection connection) noexcept {
   static const auto triggerReq = "CREATE TRIGGER IF NOT EXISTS has_album_present AFTER UPDATE OF "
     "is_present ON " + policy::AlbumTable::Name +
     " BEGIN "
-    " UPDATE " + policy::ArtistTable::Name + " SET is_present="
+    " UPDATE " + ArtistTable::Name + " SET is_present="
     "(SELECT COUNT(id_album) FROM " + policy::AlbumTable::Name + " WHERE artist_id=new.artist_id AND is_present=1) "
     "WHERE id_artist=new.artist_id;"
     " END";
 #ifdef USE_FTS
   static const auto ftsInsertTrigger = "CREATE TRIGGER IF NOT EXISTS insert_artist_fts"
-    " AFTER INSERT ON " + policy::ArtistTable::Name +
+    " AFTER INSERT ON " + ArtistTable::Name +
     " BEGIN"
-    " INSERT INTO " + policy::ArtistTable::Name + "Fts(rowid,name) VALUES(new.id_artist, new.name);"
+    " INSERT INTO " + ArtistTable::Name + "Fts(rowid,name) VALUES(new.id_artist, new.name);"
     " END";
   static const auto ftsDeleteTrigger = "CREATE TRIGGER IF NOT EXISTS delete_artist_fts"
-    " BEFORE DELETE ON " + policy::ArtistTable::Name +
+    " BEFORE DELETE ON " + ArtistTable::Name +
     " BEGIN"
-    " DELETE FROM " + policy::ArtistTable::Name + "Fts WHERE rowid=old.id_artist;"
+    " DELETE FROM " + ArtistTable::Name + "Fts WHERE rowid=old.id_artist;"
     " END";
 #endif
 
@@ -236,7 +235,7 @@ bool Artist::CreateTriggers(DBConnection connection) noexcept {
 bool Artist::createDefaultArtists(DBConnection dbConnection) {
   // Don't rely on Artist::create, since we want to insert or do nothing here.
   // This will skip the cache for those new entities, but they will be inserted soon enough anyway.
-  static const auto req = "INSERT OR IGNORE INTO " + policy::ArtistTable::Name +
+  static const auto req = "INSERT OR IGNORE INTO " + ArtistTable::Name +
     "(id_artist) VALUES(?),(?)";
   sqlite::Tools::ExecuteInsert(dbConnection, req, UnknownArtistID, VariousArtistID);
   // Always return true. The insertion might succeed, but we consider it a failure when 0 row
@@ -246,17 +245,16 @@ bool Artist::createDefaultArtists(DBConnection dbConnection) {
 
 std::shared_ptr<Artist> Artist::Create(MediaExplorerPtr ml, const std::string &name) {
   auto artist = std::make_shared<Artist>(ml, name);
-  static const auto req = "INSERT INTO " + policy::ArtistTable::Name +
-    "(id_artist, name) VALUES(NULL, ?)";
+  static const auto req = "INSERT INTO " + ArtistTable::Name + "(id_artist, name) VALUES(NULL, ?)";
   if(insert(ml, artist, req, name) == false)
     return nullptr;
   return artist;
 }
 
 std::vector<ArtistPtr> Artist::Search(MediaExplorerPtr ml, const std::string& name) {
-  static const auto req = "SELECT * FROM " + policy::ArtistTable::Name + " WHERE "
+  static const auto req = "SELECT * FROM " + ArtistTable::Name + " WHERE "
 #ifdef USE_FTS
-    "id_artist IN (SELECT rowid FROM " + policy::ArtistTable::Name + "Fts WHERE name MATCH ?)"
+    "id_artist IN (SELECT rowid FROM " + ArtistTable::Name + "Fts WHERE name MATCH ?)"
 #else
     "name LIKE '%?%' "
 #endif
@@ -269,8 +267,7 @@ std::vector<ArtistPtr> Artist::Search(MediaExplorerPtr ml, const std::string& na
 }
 
 std::vector<ArtistPtr> Artist::ListAll(MediaExplorerPtr ml, SortingCriteria sort, bool desc) {
-  auto req = "SELECT * FROM " + policy::ArtistTable::Name +
-    " WHERE nb_albums > 0 AND is_present = 1 ORDER BY ";
+  auto req = "SELECT * FROM " + ArtistTable::Name + " WHERE nb_albums > 0 AND is_present = 1 ORDER BY ";
     req += "name";
   if(desc == true)
     req += " DESC";
